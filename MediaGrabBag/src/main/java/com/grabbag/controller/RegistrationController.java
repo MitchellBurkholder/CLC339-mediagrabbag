72416bc832e234909a84b0fe89096dbeabddeb42
MitchellBurkholderGCU <stinkbugranger@proton.me>
2026-05-24 21:38:54 -0400

    Changes Have been made

---
+++ MediaGrabBag/src/main/java/com/grabbag/controller/RegistrationController.java
@@ -0,0 +1,32 @@
+package com.grabbag.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.grabbag.model.RegistrationModel;
+
+@Controller
+@RequestMapping("/mediagrabbag.com/register")
+public class RegistrationController {
+	
+	@GetMapping("/")
+	public String displayRegisterForm(Model model) {
+		model.addAttribute("title", "Registration Form");
+		model.addAttribute("Registration Model", new RegistrationModel());
+		return "register";
+	}
+	
+	@GetMapping("/")
+	public String completeRegistration(RegistrationModel registerModel, BindingResult bindingResult, Model model ) {
+		
+		
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("title", "Registration Form");
+			return "register";
+		}
+		
+		return "login";
+	}
+}
