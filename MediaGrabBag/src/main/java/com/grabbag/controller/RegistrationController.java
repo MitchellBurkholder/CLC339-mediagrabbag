fb06dbc071bfe014332a419a11acdc6d89fcb450
Jacob Cutler <cutlerjacob18@gmail.com>
2026-05-30 10:04:28 -0700

    Integrate RegistrationService in RegistrationController
    Refactor RegistrationController to use RegistrationServiceInterface for user registration.

--- MediaGrabBag/src/main/java/com/grabbag/controller/RegistrationController.java
+++ MediaGrabBag/src/main/java/com/grabbag/controller/RegistrationController.java
@@ -7,33 +7,42 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.grabbag.business.RegistrationServiceInterface;
 import com.grabbag.model.RegistrationModel;
 
 import jakarta.validation.Valid;
 
-// these annotations make the class a controller & gives it the mapping below
 @Controller
 @RequestMapping("/register")
 public class RegistrationController {
-	
-	//maps the url for the html page & setups model for data entry
-	@GetMapping("/")
-	public String displayRegisterForm(Model model) {
-		model.addAttribute("title", "Registration Form");
-		model.addAttribute("registrationModel", new RegistrationModel());
-		return "Registration";
-	}
-	
-	//process registration info
-	@PostMapping("/doRegistration")
-	public String completeRegistration(@Valid @ModelAttribute RegistrationModel registrationModel, BindingResult bindingResult, Model model ) {
-		
-		// checks for errors in data added
-		if(bindingResult.hasErrors()) {
-			model.addAttribute("title", "Registration Form");
-			return "Registration";
-		}
-		
-		return "redirect:/login/";
-	}
+
+    private final RegistrationServiceInterface registrationService;
+
+    public RegistrationController(RegistrationServiceInterface registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @GetMapping("/")
+    public String displayRegisterForm(Model model) {
+        model.addAttribute("title", "Registration Form");
+        model.addAttribute("registrationModel", new RegistrationModel());
+        return "Registration";
+    }
+
+    @PostMapping("/doRegistration")
+    public String completeRegistration(
+            @Valid @ModelAttribute("registrationModel") RegistrationModel registrationModel,
+            BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("title", "Registration Form");
+            return "Registration";
+        }
+
+        registrationService.registerUser(registrationModel);
+
+        return "redirect:/login/";
+    }
 }
