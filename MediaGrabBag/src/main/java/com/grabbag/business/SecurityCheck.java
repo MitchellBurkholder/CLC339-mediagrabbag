025a0493eb3aec94b403f1aad6244352ab2c729c
MitchellBurkholderGCU <stinkbugranger@proton.me>
2026-05-30 23:36:03 -0400

    replace the model used in registrationService.registerUser & add login authentication

--- MediaGrabBag/src/main/java/com/grabbag/business/SecurityCheck.java
+++ MediaGrabBag/src/main/java/com/grabbag/business/SecurityCheck.java
@@ -3,7 +3,7 @@ package com.grabbag.business;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SecurityCheck {
+public class SecurityCheck implements LoginInterface{
 	public boolean authenticate(String username, String password) {
 		if(username.equals("admin") && password.equals("123")) {
 			return true;
