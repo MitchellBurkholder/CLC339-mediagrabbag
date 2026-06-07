410ccd9c52aa93c45159023f125049ab38b01e8c
Jacob Cutler <cutlerjacob18@gmail.com>
2026-06-06 23:45:17 -0700

    Refactor RegistrationService to use data service

--- MediaGrabBag/src/main/java/com/grabbag/business/RegistrationService.java
+++ MediaGrabBag/src/main/java/com/grabbag/business/RegistrationService.java
@@ -2,15 +2,20 @@ package com.grabbag.business;
 
 import org.springframework.stereotype.Service;
 
+import com.grabbag.data.RegistrationDataServiceInterface;
 import com.grabbag.model.RegistrationModel;
 
 @Service
-public class RegistrationService implements com.grabbag.business.RegistrationServiceInterface {
+public class RegistrationService implements RegistrationServiceInterface {
+
+    private final RegistrationDataServiceInterface registrationDataService;
+
+    public RegistrationService(RegistrationDataServiceInterface registrationDataService) {
+        this.registrationDataService = registrationDataService;
+    }
 
     @Override
-    public void registerUser(RegistrationModel user) {
-    	//TODO: add code below to store registration info inside of the database.
-    	
-        System.out.println("Registered user: " + user.getUsername());
+    public void registerUser(RegistrationModel registrationModel) {
+        registrationDataService.createUser(registrationModel);
     }
 }
