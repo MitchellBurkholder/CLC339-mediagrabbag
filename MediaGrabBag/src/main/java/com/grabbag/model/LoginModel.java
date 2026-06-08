711a196608c5639f7f8ac71e5d6220c66989c6cf
MitchellBurkholderGCU <stinkbugranger@proton.me>
2026-06-07 21:14:38 -0400

    Login & Registration Ready to go

--- MediaGrabBag/src/main/java/com/grabbag/model/LoginModel.java
+++ MediaGrabBag/src/main/java/com/grabbag/model/LoginModel.java
@@ -1,45 +1,17 @@
 package com.grabbag.model;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
-
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-@Table("logininfo")
 public class LoginModel {
-	
-	@Id
-	private long id;
 
 	@NotNull(message="User name is required field")
 	@Size(min=1, max=32, message="User name must be bewteen 1 & 32 characters")
-	@Column("USERNAME")
 	private String username;
 	
 	@NotNull(message="Password is required field")
 	@Size(min=1, max=32, message="Password must be bewteen 1 & 32 characters")
-	@Column("PASSWORD")
 	private String password;
-	
-	//empty constructor 
-	public LoginModel() {
-		
-	}
-	
-	public LoginModel(long id, String username, String password) {
-		this.id = id;
-		this.username = username;
-		this.password = password;
-	}
-	
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
 	
 	public String getUsername() {
 		return username;
